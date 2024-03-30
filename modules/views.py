cb94138110913451a2555cce51ffc0f3cafc7e0b
nago3 <hiro@nago3.xyz>
2024-03-30 18:43:45 +0900

    add .env.template and config.py files, import HELLO variable in views.py

--- modules/views.py
+++ modules/views.py
@@ -1,12 +1,13 @@
 """views.py is a file that contains all the routes for the application"""
+from config import HELLO
 from modules import app
 
 
 @app.route('/')
 def index():
     """This is the index route
-    """
-    return 'Say Hello!!'
+    """  # Import the HELLO variable from the config module
+    return HELLO  # Use the imported HELLO variable
 
 @app.route('/about')
 def about():
