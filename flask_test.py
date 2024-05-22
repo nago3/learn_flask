3a1db6459d584177a3f0498c778ae41e8fd4d035
nago3 <hiro@nago3.xyz>
2024-05-22 18:16:00 +0900

    add: test route converter

--- flask_test.py
+++ flask_test.py
@@ -4,8 +4,21 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def index1():
     return 'Hello World!'
+
+@app.route('/param/<name>')
+def index2(name):
+    return f'Hello, {name}'
+
+@app.route('/user2/<int:id>')
+def index3(id):
+    return f'YOUR ID: {id}'
+
+@app.route('/user3', methods=['GET','POST'])
+def index4():
+    return 'GET or POST'
+
 
 if __name__ == '__main__':
     app.debug = True
